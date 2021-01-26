fa481330d3c4924f1d6e656c5db6dc7a1b99a02e
Amanda Ghassaei <amandaghassaei@gmail.com>
2021-01-25 19:06:02 -0800

    key handlers, cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,52 +2,146 @@
 import CCapture from 'ccapture.js';
 import { showAlert, showDot } from './modals';
 
-const VERBOSE = true;
+let VERBOSE = true;
 const WORKERS_PATH = '/';
 
 let capturer: CCapture | null = null;
 
-export function beginVideoRecord(options: {
+let videoRecKey: string | null = null;
+let gifRecKey: string | null = null;
+let pngRecKey: string | null = null;
+
+let isRecordingVideo = false;
+let isRecordingGIF = false;
+
+let canvas: HTMLCanvasElement | null = null;
+
+let numFrames = 0;
+
+export function init(_canvas: HTMLCanvasElement) {
+	canvas = _canvas;
+}
+
+export function setVerbose(state: boolean) {
+	VERBOSE = !!state;
+}
+
+function checkCanvas() {
+	if (canvas === null) {
+		showAlert('No canvas supplied, please call CanvasCapture.init() and pass in canvas element.');
+		return false;
+	}
+	return true;
+}
+
+window.addEventListener('keydown', (e: KeyboardEvent) => {
+	if (videoRecKey && e.key === videoRecKey) {
+		beginVideoRecord();
+	}
+	if (gifRecKey && e.key === gifRecKey) {
+		beginGIFRecord();
+	}
+	if (pngRecKey && e.key === pngRecKey) {
+		takePNGSnapshot();
+	}
+});
+
+// Pressing key once will start record, press again to stop.
+export function bindKeyToVideoRecord(key: string) {
+	if (key === gifRecKey) {
+		gifRecKey = null;
+	}
+	if (key === pngRecKey) {
+		pngRecKey = null;
+	}
+	videoRecKey = key;
+}
+export function bindKeyToGIFRecord(key: string) {
+	if (key === videoRecKey) {
+		videoRecKey = null;
+	}
+	if (key === pngRecKey) {
+		pngRecKey = null;
+	}
+	gifRecKey = key;
+}
+export function bindKeyToPNGSnapshot(key: string) {
+	if (key === gifRecKey) {
+		gifRecKey = null;
+	}
+	if (key === videoRecKey) {
+		videoRecKey = null;
+	}
+	pngRecKey = key;
+}
+
+export function beginVideoRecord(options?: {
 	fps?: number,
 	name?: string,
 	quality?: string,
 }) {
-	clearCapturer();
+	if (isRecordingGIF) {
+		showAlert('You are currently recording a gif, stop recording gif before starting new video record.');
+		return;
+	}
+	if (isRecordingVideo) {
+		showAlert('You are currently recording a video, stop recording current video before starting new video record.');
+		return;
+	}
 	// Create a capturer that exports a WebM video
 	capturer = new CCapture( {
 		format: 'webm',
-		name: options.name || 'WEBM_Capture',
-		framerate: options.fps || 60,
-		quality: options.quality || 63,
+		name: options?.name || 'WEBM_Capture',
+		framerate: options?.fps || 60,
+		quality: options?.quality || 63,
 		verbose: VERBOSE,
 	});
+	isRecordingVideo = true;
 	startRecord();
 }
 
-export function beginGifRecord(options: {
+export function beginGIFRecord(options?: {
 	fps?: number,
 	name?: string,
 }) {
-	clearCapturer();
+	if (isRecordingVideo) {
+		showAlert('You are currently recording a video, stop recording video before starting new gif record.');
+		return;
+	}
+	if (isRecordingGIF) {
+		showAlert('You are currently recording a gif, stop recording current gif before starting new gif record.');
+		return;
+	}
 	// Create a capturer that exports a WebM video
 	capturer = new CCapture({
 		format: 'gif',
-		name: options.name || 'GIF_Capture',
-		framerate: options.fps || 60,
+		name: options?.name || 'GIF_Capture',
+		framerate: options?.fps || 60,
 		workersPath: WORKERS_PATH,
 		verbose: VERBOSE,
 	});
+	isRecordingGIF = true;
 	startRecord();
 }
 
-export function takePNGSnapshot(canvas: HTMLCanvasElement, options: {
+export function takePNGSnapshot(options?: {
 	name?: string,
 }) {
-	clearCapturer();
+	if (!checkCanvas()) {
+		return;
+	}
+	if (isRecordingVideo) {
+		showAlert('You are currently recording a video, stop recording video before starting new png snapshot.');
+		return;
+	}
+	if (isRecordingGIF) {
+		showAlert('You are currently recording a gif, stop recording gif before starting new png snapshot.');
+		return;
+	}
 	// Create a capturer that exports a WebM video
 	capturer = new CCapture({
 		format: 'png',
-		name: options.name || 'PNG_Capture',
+		name: options?.name || 'PNG_Capture',
 		verbose: VERBOSE,
 	});
 	capturer.start();
@@ -55,34 +149,40 @@ export function takePNGSnapshot(canvas: HTMLCanvasElement, options: {
 	stopRecord();
 }
 
-export function recordFrame(canvas: HTMLCanvasElement) {
+export function recordFrame() {
+	if (!checkCanvas()) {
+		return;
+	}
 	if (!capturer) {
-		throw new Error('No valid capturer inited.');
+		showAlert('No valid capturer inited, please call CanvasCapture.beginVideoRecord() or CanvasCapture.beginGIFRecord() first.');
+		return;
 	}
 	capturer.capture(canvas);
+	numFrames++;
 }
 
 function startRecord() {
 	capturer.start();
 	// For video and gif records, we should also throw up an indicator to show that we're in record mode.
 	showDot(true);
+	numFrames = 0;
 }
 
 export function stopRecord() {
 	if (!capturer) {
-		throw new Error('No valid capturer inited.');
+		showAlert('No valid capturer inited, please call CanvasCapture.beginVideoRecord() or CanvasCapture.beginGIFRecord() first.');
+		return;
+	}
+	if (numFrames === 0) {
+		showAlert('No frames recorded, call CanvasCapture.recordFrame()');
+		return;
 	}
 	capturer.stop();
 	capturer.save();
 	capturer = null;
-}
-
-function clearCapturer() {
-	if (capturer) {
-		capturer.stop();
-		capturer = null;
-		showDot(false);
-	}
+	isRecordingGIF = false;
+	isRecordingVideo = false;
+	showDot(false);
 }
 
 window.addEventListener('resize', function(){
