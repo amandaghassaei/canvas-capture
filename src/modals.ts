e729ae6339aaf1b34b6df1a70f41c8e692b7275e
Amanda Ghassaei <amandaghassaei@gmail.com>
2021-01-28 23:35:55 -0800

    modal fixes

--- src/modals.ts
+++ src/modals.ts
@@ -12,19 +12,19 @@ function initModalHTML(modalID: string, title: string, content: string = '') {
 		<div class="modal__overlay" tabindex="-1" data-micromodal-close>
 		<div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-${modalID}-title">
 			<header class="modal__header">
-			<h2 class="modal__title" id="modal-${modalID}-title">
-				${title}
-			</h2>
-			<button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
+				<h2 class="modal__title" id="modal-${modalID}-title">
+					${title}
+				</h2>
+				<button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
 			</header>
-			<main class="modal__content" id="modal-${modalID}-content">
-			<p>
-			${content}
-			</p>
+			<main class="modal__content">
+				<p id="modal-${modalID}-content>
+					${content}
+				</p>
 			</main>
 			<footer class="modal__footer">
-			<button class="modal__btn modal__btn-primary">Continue</button>
-			<button class="modal__btn" data-micromodal-close aria-label="Close this dialog window">Close</button>
+				<button class="modal__btn modal__btn-primary">Continue</button>
+				<button class="modal__btn" data-micromodal-close aria-label="Close this dialog window">Close</button>
 			</footer>
 		</div>
 		</div>
@@ -44,7 +44,7 @@ MicroModal.init();
 
 
 export function showAlert(message: string) {
-	(document.getElementById(`modal-${ALERT_MODAL_ID}-content`) as HTMLDivElement).innerHTML = message;
+	(document.getElementById(`modal-${ALERT_MODAL_ID}-content`) as HTMLElement).innerHTML = message;
 	MicroModal.show(`modal-${ALERT_MODAL_ID}`);
 }
 
